9af6766b1819b3dadc91ebc00dada0f6591312ce
Aman Kumar <amankumar230891@gmail.com>
2023-09-26 21:32:16 +0530

    Create event handler generators for 'data' and 'end' events on active socket

--- src/index.js
+++ src/index.js
@@ -2,17 +2,30 @@ const net = require("net");
 const { PORT_NUMBER, ADDRESS, BACKLOG } = require("./constants");
 const { getSocketKey } = require("./utils");
 
+// returns active socket specific handler for "data" event
+const getHandleClientData = (socket) => {
+    const handleClientData = (data) => {
+        console.log(`${getSocketKey(socket)} sent message: ${data}`);
+    };
+    return handleClientData;
+}
+
+// returns active socket specific handler for "end" event
+const getHandleClientDisconnect = (socket) => {
+    const handleClientDisconnect = () => {
+        console.log(`${getSocketKey(socket)} is now disconnected`);
+    }
+    return handleClientDisconnect;
+}
+
+
 const handleClientConnection = (newActiveSocket) => {
 
     console.log(`${getSocketKey(newActiveSocket)} is now connected`);
 
-    newActiveSocket.on("data", (data) => {
-        console.log(`${getSocketKey(newActiveSocket)} sent message: ${data}`);
-    });
-
-    newActiveSocket.on("end", () => {
-        console.log(`${getSocketKey(newActiveSocket)} is now disconnected`)
-    })
+    // adding event listeners to active socket
+    newActiveSocket.on("data", getHandleClientData(newActiveSocket));
+    newActiveSocket.on("end", getHandleClientDisconnect(newActiveSocket));
 
 }
 
