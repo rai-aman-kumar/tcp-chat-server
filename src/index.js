1c08775a667db65f4e89a46b7d451b36ba34c096
Aman Kumar <amankumar230891@gmail.com>
2023-09-26 21:10:13 +0530

    Add connection event listener to passive socket

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const net = require("net");
 
 
+const handleClientConnection = () => {
+    console.log("Client connected");
+}
+
 const server = net.createServer();
+server.on("connection", handleClientConnection);
 
 server.listen(3000, '127.0.0.1', 5, () => {
     console.log("Server started")
